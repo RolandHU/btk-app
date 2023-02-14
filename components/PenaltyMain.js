b15510d7a0edd30aedb5a7f6ac3caaa3dbd71896
RolandHU <kriporkripor@gmail.com>
2023-02-14 16:15:31 +0100

    PenaltyMain component build quality changes

--- components/PenaltyMain.js
+++ components/PenaltyMain.js
@@ -3,18 +3,13 @@ import Penalty from "./Penalty"
 
 export default function PenaltyMain({ elem, data, func }) {
   return (
-    <div className="border border-slate-700 rounded-md overflow-hidden">
-      <div className="px-10 py-5 bg-slate-800" onClick={() => func.changePenaltyState(elem.Paragraph)}>
-        <h2 className="text-xl font-semibold">{elem.Name}</h2>
-      </div>
-      {
-        func.penaltyState.includes(elem.Paragraph) ?
-          <div className="flex flex-col gap-2.5 p-5 border-t border-slate-700">
-            {data.map(d => <Penalty key={uuid4()} data={d} func={func} />)}
-          </div>
-        :
-          null
-      }
-    </div>
+    <details className="border border-slate-700 rounded-md overflow-clip" open={func.penaltyState.includes(elem.Paragraph)}>
+      <summary className="list-none">
+        <h2 className="px-10 py-5 text-xl font-semibold bg-slate-800" onClick={() => func.changePenaltyState(elem.Paragraph)}>{elem.Name}</h2>
+      </summary>
+      <section className="flex flex-col gap-2.5 p-5">
+        {data.map(d => <Penalty key={uuid4()} data={d} func={func} />)}
+      </section>
+    </details>
   )
 }
